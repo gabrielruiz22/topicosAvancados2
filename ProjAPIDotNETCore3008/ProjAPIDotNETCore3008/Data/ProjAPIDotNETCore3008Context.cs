359471a5e5fe7bfac0ad612867f8f0db530efe88
Gabriel Ruiz <gabriel.eliaz@hotmail.com>
2021-08-30 21:48:01 -0300

    exercicio 3008

---
+++ ProjAPIDotNETCore3008/ProjAPIDotNETCore3008/Data/ProjAPIDotNETCore3008Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjAPIDotNETCore3008.Model;
+
+namespace ProjAPIDotNETCore3008.Data
+{
+    public class ProjAPIDotNETCore3008Context : DbContext
+    {
+        public ProjAPIDotNETCore3008Context (DbContextOptions<ProjAPIDotNETCore3008Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjAPIDotNETCore3008.Model.Person> Person { get; set; }
+    }
+}
---
+++ ProjAPIDotNETCore3008/ProjAPIDotNETCore3008/Model/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjAPIDotNETCore3008.Model
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public String Telephone { get; set; }
+    }
+}
