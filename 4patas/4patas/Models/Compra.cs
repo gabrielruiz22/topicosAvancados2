5b014fd560db49d34002bfc2d49274cfb563e79e
Gabriel Ruiz <gabriel.eliaz@hotmail.com>
2021-10-25 20:44:00 -0300

    4patas p2

---
+++ 4patas/4patas/Models/Compra.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _4patas.Models
+{
+    public class Compra
+    {
+        public int Id { get; set; }
+        public Produto NomeProduto { get; set; }
+        public Usuario NomeUsuario { get; set; }
+        public Funcionario NomeFuncionario { get; set; }
+    }
+}
